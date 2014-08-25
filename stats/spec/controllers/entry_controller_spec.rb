8a5866428862e2dd034a5c7629ff2d12f4ed7398
Lucas <lucasapava@gmail.com>
2014-08-25 17:32:14 -0300

    Estamos fazendo a questao 6

--- stats/spec/controllers/entry_controller_spec.rb
+++ stats/spec/controllers/entry_controller_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
 describe Entry do
+	before :each do
+		@entry = Entry.create(label: "exemplo", sequence: "1,2,3", author: "cavalo")
+	end
 	it "create an entry and verify attributes" do
-	
+    	#entry.id_entry.should == 1
+    	expect (@entry.label).to eq("exemplo")
+    	expect (@entry.sequence).to eq("1,2,3")
+    	expect (@entry.author).to eq("cavalo")
+    	#@entry.label.should == "exemplo"
+    	#@entry.sequence.should == "aaaaa"
+    	#@entry.author.should == "cavalo"
 
 	end 
 	
