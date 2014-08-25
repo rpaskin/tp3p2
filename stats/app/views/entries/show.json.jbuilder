7175526335988512b04ff535f214f4b0c2238e5a
Lucas <lucasapava@gmail.com>
2014-08-25 16:31:45 -0300

    Agora as questoes 4 e 5 foram feitas

---
+++ stats/app/views/entries/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @entry, :id, :label, :sequence, :author, :created_at, :updated_at
---
+++ stats/db/migrate/20140825192832_create_entries.rb
@@ -0,0 +1,11 @@
+class CreateEntries < ActiveRecord::Migration
+  def change
+    create_table :entries do |t|
+      t.string :label
+      t.string :sequence
+      t.string :author
+
+      t.timestamps
+    end
+  end
+end
