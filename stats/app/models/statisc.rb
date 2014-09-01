63a94d19a36adbd6ac71645450ea3b28ece29b13
Marcus Vinnicius Ribeiro Madureira <mvmadu@gmail.com>
2014-09-01 17:24:01 -0300

    To vendo nada aqui
    Flw Valeu leskão

--- stats/app/models/statisc.rb
+++ stats/app/models/statisc.rb
@@ -1,12 +1,1 @@
-class Statisc < ActiveRecord::Base
-#DEZ MULEKKKKKKK
-  after_initialize do
-    self.max = self.sequence.split.collect{|i| i.to_i}.max
-    self.min = self.sequence.split.collect{|i| i.to_i}.min
-    self.sum = self.sequence.split.collect{|i| i.to_i}.sum
-    self.avg = self.sum/self.sequence.split.size
-    self.lastName = author.split.last
-    puts self.inspect
-  end
-
-end
+HUEHUEHEUH
