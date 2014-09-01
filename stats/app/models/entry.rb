3a8500280805a5e68df0c9dd13fe00b09d1b0e53
Lucas <lucasapava@gmail.com>
2014-09-01 17:19:57 -0300

    Questões 7 e 9 feitas

--- stats/app/models/entry.rb
+++ stats/app/models/entry.rb
@@ -1,3 +1,13 @@
 class Entry < ActiveRecord::Base
-	
+	after_initialize do 
+		split_sequence = self.sequence.split(",")
+		self.max = split_sequence.map(&:to_i).max
+		self.min = split_sequence.map(&:to_i).min
+		self.sum = split_sequence.map(&:to_i).inject{ |sum, el| sum + el }
+		self.average = self.sum.to_f / split_sequence.size
+
+		split_author = self.author.split
+		self.lastName = split_author.last
+		
+	end
 end
--- stats/spec/controllers/entry_controller_spec.rb
+++ stats/spec/controllers/entry_controller_spec.rb
@@ -2,20 +2,13 @@ require 'spec_helper'
 
 describe Entry do
 	before :each do
-		@entry = Entry.new(label: "exemplo", sequence: "1,2,3", author: "cavalo")#, max: 1, min: 2, average: 1.5, sum: 1, lastName: "foca")
+		@entry = Entry.new(label: "exemplo", sequence: "1,2,3", author: "cavalo foca")
 	end
 
 	it "create an entry and verify attributes" do
     	expect(@entry.label).to eq("exemplo")
     	expect(@entry.sequence).to eq("1,2,3")
-    	expect(@entry.author).to eq("cavalo")
-    	#expect(@entry.max).to eq(1)
-    	#expect(@entry.min).to eq(2)
-    	#expect(@entry.average).to eq(1.5)
-    	#expect(@entry.sum).to eq(1)
-    	#expect(@entry.lastName).to eq("foca")
-
-
+    	expect(@entry.author).to eq("cavalo foca")
 
 
 
@@ -23,7 +16,16 @@ describe Entry do
 	
 	it 	"destroy an entry and verify if it was destroyed" do
 		@entry.destroy
-		expect(Entry.find_by(author: "cavalo")).to eq(nil)
+		expect(Entry.find_by(author: "cavalo foca")).to eq(nil)
+
+	end
+
+	it "verify max, min, average, sum and lastName fields" do
+		expect(@entry.max).to eq(3)
+		expect(@entry.min).to eq(1)
+    	expect(@entry.average).to eq(2)
+    	expect(@entry.sum).to eq(6)
+    	expect(@entry.lastName).to eq("foca")
 
 	end
 end
