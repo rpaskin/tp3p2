bd6fc77ef91d20d278d7716001ea4b104f78d964
Ronnie Paskin <ronnie@megaputz.com>
2014-09-02 09:25:53 -0300

    Resolve problema na inicialização sem valores

--- stats/app/models/statisc.rb
+++ stats/app/models/statisc.rb
@@ -1,12 +1,15 @@
 class Statisc < ActiveRecord::Base
 #DEZ MULEKKKKKKK
   after_initialize do
-    self.max = self.sequence.split.collect{|i| i.to_i}.max
-    self.min = self.sequence.split.collect{|i| i.to_i}.min
-    self.sum = self.sequence.split.collect{|i| i.to_i}.sum
-    self.avg = self.sum/self.sequence.split.size
-    self.lastName = author.split.last
-    puts self.inspect
-  end
+    unless self.sequence.blank?
+      self.max = self.sequence.split.collect{|i| i.to_i}.max
+      self.min = self.sequence.split.collect{|i| i.to_i}.min
+      self.sum = self.sequence.split.collect{|i| i.to_i}.sum
+      self.avg = self.sum/self.sequence.split.size
+    end
 
+    unless author.blank?
+      self.lastName = author.split.last
+    end
+  end
 end
