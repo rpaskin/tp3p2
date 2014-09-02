7a0aa5c4c07b26e81b0f5f2581cc54e7eb7b4648
Ronnie Paskin <ronnie@megaputz.com>
2014-09-02 09:19:56 -0300

    Revert "To vendo nada aqui"
    This reverts commit 63a94d19a36adbd6ac71645450ea3b28ece29b13.

--- stats/app/models/statisc.rb
+++ stats/app/models/statisc.rb
@@ -1,1 +1,12 @@
-HUEHUEHEUH
+class Statisc < ActiveRecord::Base
+#DEZ MULEKKKKKKK
+  after_initialize do
+    self.max = self.sequence.split.collect{|i| i.to_i}.max
+    self.min = self.sequence.split.collect{|i| i.to_i}.min
+    self.sum = self.sequence.split.collect{|i| i.to_i}.sum
+    self.avg = self.sum/self.sequence.split.size
+    self.lastName = author.split.last
+    puts self.inspect
+  end
+
+end
